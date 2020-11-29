041c827b062d107e260b95abfd0746cd82b68b15
Nikita Igumnov <nkigumnov@edu.hse.ru>
2020-11-30 01:50:03 +0300

    main update (hw 30.11.2020)

---
+++ CommitsApp/CommitsApp/Author+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Author+CoreDataClass.swift
+//  CommitsApp
+//
+//  Created by Никита Игумнов on 29.11.2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Author)
+public class Author: NSManagedObject {
+
+}
